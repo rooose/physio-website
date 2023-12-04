69c11ea494ebb5edfda9c9418520957a2927f34f
ThierryDubois <thierry.dubois05@gmail.com>
2023-12-04 12:17:32 -0500

    min-w

--- physioWebsite/src/components/header.tsx
+++ physioWebsite/src/components/header.tsx
@@ -10,20 +10,20 @@ class Header extends Component {
     return (
       <header className="bg-zinc-300">
         <div id="header-top">
-          <nav id="nav-wrap" className="px-4 min-w-fit w-1/2">
+          <nav id="nav-wrap" className="pl-4 min-w-fit w-1/2">
             <span className="left-nav">
               <a>Fr</a>|<a>En</a>
             </span>
             <ul id="nav" className="right-nav">
-              <li>
+              <li className="min-w-fit">
                 <a href="tel:514-843-3733">(514) 843-3733</a>
               </li>
-              <li>
+              <li className="min-w-fit">
                 <a href="mailto:iguevin.physio@gmail.com">
                   iguevin.physio@gmail.com
                 </a>
               </li>
-              <li>
+              <li className="min-w-fit">
                 <a
                   href="https://maps.app.goo.gl/mZLBSqb9vCC4wWhz8"
                   target="_blank"
