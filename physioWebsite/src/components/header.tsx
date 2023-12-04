db3287ec0496fab24d5697814681960f1efe1d5b
ThierryDubois <thierry.dubois05@gmail.com>
2023-12-04 12:14:53 -0500

    header layout
    responsive layout

--- physioWebsite/src/components/header.tsx
+++ physioWebsite/src/components/header.tsx
@@ -1,30 +1,40 @@
-import React, {Component} from 'react';
-import './header.css'
+import React, { Component } from "react";
+import "./header.css";
 
 class Header extends Component {
+  constructor(props: any) {
+    super(props);
+  }
 
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-      return (
-            <header>
-                <div id="header-top">
-                    <nav id="nav-wrap">
-                        <span className="left-nav">
-                            <a>Fr</a> 
-                            |
-                            <a>En</a>
-                        </span>
-                        <ul id="nav" className="right-nav">
-                            <li><a href="tel:123-456-7890">(123) 456-769</a></li>
-                            <li><a href="mailto:email@addresse.com">email@addresse.com</a></li>
-                            <li><a href="https://maps.app.goo.gl/mZLBSqb9vCC4wWhz8" target="_blank">350 Bd Saint-Joseph E</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                {/* <div id="header-top">
+  render() {
+    return (
+      <header className="bg-zinc-300">
+        <div id="header-top">
+          <nav id="nav-wrap" className="px-4 min-w-fit w-1/2">
+            <span className="left-nav">
+              <a>Fr</a>|<a>En</a>
+            </span>
+            <ul id="nav" className="right-nav">
+              <li>
+                <a href="tel:514-843-3733">(514) 843-3733</a>
+              </li>
+              <li>
+                <a href="mailto:iguevin.physio@gmail.com">
+                  iguevin.physio@gmail.com
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://maps.app.goo.gl/mZLBSqb9vCC4wWhz8"
+                  target="_blank"
+                >
+                  350 Bd Saint-Joseph E
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        {/* <div id="header-top">
                     <nav id="nav-below">
                         <span>
                             Isabelle Guévin
@@ -33,9 +43,9 @@ class Header extends Component {
                         </span>
                     </nav>
                 </div> */}
-            </header>
-        )
-    }
+      </header>
+    );
   }
+}
 
-  export default Header;
+export default Header;
