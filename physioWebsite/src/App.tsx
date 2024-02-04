cd7ee431b3097770b30b281489924e49aab5e55c
rooose <rosehirigoyen@gmail.com>
2024-02-04 13:39:17 -0500

    mobile disclaimer

--- physioWebsite/src/App.tsx
+++ physioWebsite/src/App.tsx
@@ -10,6 +10,8 @@ import Contact from "./components/Pages/contact";
 import Reeducation from "./components/Pages/reeducation";
 import Pediatrie from "./components/Pages/pediatrie";
 
+import {BrowserView, MobileView} from 'react-device-detect';
+
 export default function App() {
   const [index, setIndex] = useState(0);
   const { t } = useTranslation();
@@ -17,16 +19,21 @@ export default function App() {
   return (
     <div className="page-container">
       <div className="content-wrap flex flex-col max-h-full items-center">
-        <Header />
-        <Navigation setIndex={setIndex} />
-        <div className="mt-6">
-          {index == 0 && <LandingPage setIndex={setIndex} />}
-          {index == 1 && <Reeducation />}
-          {index == 2 && <Formations />}
-          {index == 3 && <Contact />}
-          {index == 4 && <Pediatrie />}
-        </div>
-        <Footer />
+        <BrowserView>
+          <Header />
+          <Navigation setIndex={setIndex} />
+          <div className="mt-6">
+            {index == 0 && <LandingPage setIndex={setIndex} />}
+            {index == 1 && <Reeducation />}
+            {index == 2 && <Formations />}
+            {index == 3 && <Contact />}
+            {index == 4 && <Pediatrie />}
+          </div>
+          <Footer />
+        </BrowserView>
+        <MobileView>
+          <h1 className="disclaimer">Site mobile en construction :)</h1>
+        </MobileView>
       </div>
     </div>
   );
